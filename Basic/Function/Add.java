7b0720c6b311cc57b982d192807c8e6f19ab4f0c
Kirubel Wondwoson <124454603+Kirubel-wondwoson@users.noreply.github.com>
2024-02-02 13:14:16 +0300

    Add files via upload

---
+++ Basic/Function/Add.java
@@ -0,0 +1,11 @@
+package Function;
+import java.util.*;
+public class Add {
+  public static double add(double n1 , double n2){
+    double sum = n1 + n2;
+    return sum;
+  }
+  public static void main(String []args){
+    add(20.4,40.1);
+  }
+}
---
+++ Basic/Function/CheckEvenOdd.java
@@ -0,0 +1,17 @@
+package Function;
+import java.util.*;
+public class CheckEvenOdd {
+  public static String checkEvenOdd(int num){
+    String display;
+    if(num%2 == 0){
+      display = num + " is even.";
+    }
+    else{
+      display = num + " is odd.";
+    }
+    return display;
+  }
+  public static void main(String []args){
+    checkEvenOdd(3);
+  }
+}
